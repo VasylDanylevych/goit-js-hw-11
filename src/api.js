dd1efba9011b726f0f80dfeacfc9dd44cb932c4f
VasylDanylevych <ukrainec31@gmail.com>
2023-02-07 22:50:04 +0200

    upd

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
+const axios = require('axios').default;
 const URL = "https://pixabay.com/api/";
 const API_KEY = "33456182-4b4e94c7d011671bbd7c07f09";
 
-function getImg(params) {
+function getImg(value) {
 
-    return axios.get(`${URL}?key=${API_KEY}&q=${params}&image_type=photo&orientation=horizontal&safesearch=true`)
+    return axios.get(`${URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`)
     .then((res) => {
         if (!res.ok) {
             throw new Error(res.status);
